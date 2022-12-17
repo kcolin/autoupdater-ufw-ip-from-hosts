60e3dad2af2738a3f88ee9b4a8814bae016d7641
kcolin <nt@alitel.biz>
2022-12-17 13:49:10 +0200

    add ufw path to config

--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@
 hosts="whitelist.domain.com;whitelist2.domain.com"
 comment_marker="awl-marked"
 ufw_path="/sbin/ufw"
+dig_path="dig"
 
 
 #delete old marked ips
-old_ips=`/sbin/ufw status | grep $comment_marker | awk '{print $3}'`
+old_ips=`$ufw_path status | grep $comment_marker | awk '{print $3}'`
 for old_ip in $old_ips
 do
    $ufw_path delete allow from $old_ip
@@ -17,7 +18,7 @@ new_ips=""
 for host in $(echo $hosts | tr ";" "\n")
 do
   new_ips+=$'\n'
-  new_ips+=$(dig +short $host)
+  new_ips+=$($dig_path +short $host)
 done
 
 #setup new ips
